3ff33d1dc309ee9da6f7fc3ee631fd382f95cfb4
Janet <Reid>
2019-12-27 15:47:09 +0800

    Fixed error in home page route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,14 @@
-//Pull in required dependencies
-
+//Pull in required dependency
 var path = require("path");
 
 //Export HTML routes
 module.exports = function(app) {
-  //Home Page
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
   //Survey Page
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+  //Home Page
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 };
